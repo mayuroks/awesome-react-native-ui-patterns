5e558b39222fc53b0d9cfd6263edec2fde9d6d07
mayuroks <mayurzenith@gmail.com>
2019-02-15 00:05:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/components/HomeScreen';
+
+export default App = () => {
+    return <HomeScreen></HomeScreen>
+}
---
+++ src/components/DetailsScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, Flatlist } from 'react-native';
+
+class DetailsScreen extends Component {
+        render() {
+            return <Text>DetailsScreen</Text>;
+        }
+    }
+
+export default DetailsScreen;
